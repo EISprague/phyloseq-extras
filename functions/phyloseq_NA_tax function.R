67cfdac5c2ee33d6eaa69df2b1cd3c69fe76fa37
EISprague <40368781+EISprague@users.noreply.github.com>
2019-08-08 15:55:12 -0500

    Update phyloseq_NA_tax function.R
    This works much faster now, but the output seems to make taxa_abundance take a long time with the GlobalPatterns dataset

--- functions/phyloseq_NA_tax function.R
+++ functions/phyloseq_NA_tax function.R
@@ -1,23 +1,21 @@
 phyloseq_NA_tax = function(physeq) {
   
+  require(phyloseq)
   out = physeq
   
-  for(row in 1:nrow(tax_table(out))) {
-    if(is.na(tax_table(out))[row, "Kingdom"]) {
-      tax_table(out)[row, 1:7] = "Unassigned_kingdom"
-    } else if(is.na(tax_table(out)[row, "Phylum"])) {
-      tax_table(out)[row, 2:7] = paste0("Kingdom_", tax_table(out)[row, "Kingdom"])
-    } else if(is.na(tax_table(out)[row, "Class"])) {
-      tax_table(out)[row, 3:7] = paste0("Phylum_", tax_table(out)[row, "Phylum"])
-    } else if(is.na(tax_table(out)[row, "Order"])) {
-      tax_table(out)[row, 4:7] = paste0("Class_", tax_table(out)[row, "Class"])
-    } else if(is.na(tax_table(out)[row, "Family"])) {
-      tax_table(out)[row, 5:7] = paste0("Order_", tax_table(out)[row, "Order"])
-    } else if(is.na(tax_table(out)[row, "Genus"])) {
-      tax_table(out)[row, 6:7] = paste0("Family_", tax_table(out)[row, "Family"])
-    } else if(is.na(tax_table(out)[row, "Species"])) {
-      tax_table(out)[row, 7] = paste0("Genus_", tax_table(out)[row, "Genus"])
-    }
-  }
+  tax_table(out)[is.na(tax_table(out)[, "Kingdom"]), ] = "Unassigned_kingdom"
+  tax_table(out)[is.na(tax_table(out)[, "Phylum"]), 2:7] = 
+    paste0("Kingdom_", tax_table(out)[is.na(tax_table(out)[, "Phylum"]), "Kingdom"])
+  tax_table(out)[is.na(tax_table(out)[, "Class"]), 3:7] = 
+    paste0("Phylum_", tax_table(out)[is.na(tax_table(out)[, "Class"]), "Phylum"])
+  tax_table(out)[is.na(tax_table(out)[, "Order"]), 4:7] = 
+    paste0("Class_", tax_table(out)[is.na(tax_table(out)[, "Order"]), "Class"])
+  tax_table(out)[is.na(tax_table(out)[, "Family"]), 5:7] = 
+    paste0("Order_", tax_table(out)[is.na(tax_table(out)[, "Family"]), "Order"])
+  tax_table(out)[is.na(tax_table(out)[, "Genus"]), 6:7] = 
+    paste0("Family_", tax_table(out)[is.na(tax_table(out)[, "Genus"]), "Family"])
+  tax_table(out)[is.na(tax_table(out)[, "Species"]), 7] = 
+    paste0("Genus_", tax_table(out)[is.na(tax_table(out)[, "Species"]), "Genus"])
+
   return(out)
 }
